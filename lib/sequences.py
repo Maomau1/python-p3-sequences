ffac0cad707773a0e3bfef580cf3177cb5a9b7f8
maurice diong <mdiong00@gmail.com>
2024-01-17 08:31:23 -0500

    completed but let's try to make it DRY

--- lib/sequences.py
+++ lib/sequences.py
@@ -2,3 +2,16 @@
 
 def print_fibonacci(length):
     pass
+    fibonacci = []
+    if length == 1 :
+        fibonacci = [0]
+    elif length ==2 :
+        fibonacci = [0,1]
+    elif length > 2 :
+        fibonacci = [0,1]
+        i=1
+        while i < length -1 :
+            fibonacci.append(fibonacci[i]+fibonacci[i-1])
+            i+=1
+    print(fibonacci)
+    
